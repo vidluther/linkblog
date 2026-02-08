e989b8f3e41c69cf6ff77e17b0c3fac5aac2a1e1
Vid Luther <vid@luther.io>
2026-02-08 18:50:52 +0530

    initial setup of database
    starting to learn things with supabase cli, and setting up local 
    db environment. 
    
    affects #2

---
+++ supabase/seed.sql
@@ -0,0 +1,5 @@
+insert into "links" ("id", "url", "title", "summary", "created_at", "updated_at")
+values (1, 'https://simonwillison.net/2026/Feb/2/introducing-the-codex-app/', 'OpenAI Codex Announcement', 'This is a summary', now(), now());
+
+insert into "links" ("id", "url", "title", "summary", "created_at", "updated_at")
+values (2, 'https://daringfireball.net/linked/2026/01/29/amazon-melania-spending', 'Legalized Bribes?', 'This is another summary', now(), now());
